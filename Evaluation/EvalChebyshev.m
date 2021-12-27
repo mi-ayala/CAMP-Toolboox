3b42ce68cd7269d9f54992352e82210e7b9abd06
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2021-12-27 15:26:52 -0500

    ChebEval update

--- Evaluation/EvalChebyshev.m
+++ Evaluation/EvalChebyshev.m
@@ -1,26 +1,40 @@
-function  f = EvalChebyshev(TaylorCoeff,t)
+function  f = EvalChebyshev(chebcoeffs,t)
     %%%% General Chebyshev evaluation Function at times t
     % ================================================
     % Nov 2021, Miguel Ayala.
     % ================================================
     
-        % x est un vecteur colonne et n un entier plus grand ou egal à 0
-        %fonction qui évalue recursivement T_0,T_1,....T_n où T sont les polynomes de Chebychev
-        %en utilisant la formule de recurrence: T_0=1, T_1=x et 
-        %T_n= 2*x*T_(n-1) -T_(n-2)
-        % la matrice X  est sous la forme [T_0(x),T_1(x),...,T_n(x)]
-        
-        if n==0
-            M=ones(length(y),1);        
-        end
-        if n==1
-            M=[ones(length(y),1),y];
-        end
-        if n>=2
-            M1=evalCheby(y,n-1); %la derniere colonne sera T_n-1 et l'avant dernière T_n-2 ?????
-            M= [M1, 2*y.*M1(:,end)-M1(:,end-1)];
-        end
+        %% Inputs Cheb coeffs. 
+        %% Evaluation point in [-1,1]. It could be a row. 
+        %% Two options, Chebfn or symbolic toolbox
+        %% Note the 2 factor in front of all expansion terms > 0
+    
+        t =reshape(t,[],1);
+        chebcoeffs = reshape(chebcoeffs,1,[]);
 
+        M = length(chebcoeffs) - 1;
+        t_evaluations = length(t);
         
+        
+        %%% ChebCoeffs
+        ChebC = repmat( chebcoeffs(2:end) , 3,1);
+        
+        %%% Chebpolynomials
+        
+        ChebP = zeros(t_evaluations, M);
+        
+         for i = 1: M
+        
+        %   %%% Using Chebfun
+        %      T = chebpoly(i);
+        %      ChebP(:,i) = T(t);
+            
+            %%% Using symbolic toolbox
+             ChebP(:,i) = chebyshevT(i,t);
+             
+         end    
+        
+        R = chebcoeffs(1) + 2*ChebC.*ChebP
+       
     
 end
