844fb9867ec9ad099c8c25fd7730f1c4fc9bd8c6
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-01-05 16:11:04 -0500

    Invertibility test function

---
+++ Derivatives/InvertibilityTest.m
@@ -0,0 +1,32 @@
+function  [nullity_QR,null_space] = InvertibilityTest(A)
+%  ==================================================
+%  InvertibilityTest
+%  ==================================================
+%  DESCRIPTION  Computes condition number and kernel
+%  __________________________________________________
+%  INPUT        Square matrix
+%  __________________________________________________
+%  OUTPUT       Condition number and basis for 
+%               the kernel.
+%  __________________________________________________
+%  MATLAB 9.11.0.1809720 (R2021b) Update 1
+%  Miguel Ayala, 05-Jan-2022.
+%  ==================================================
+
+    %%% How close to zero are the zero eigenvalues?
+    tol = 1e-6;
+
+    %%% Condition Number
+    condition_Number = cond(A)^-1
+    
+    %%% Eig 
+    [V,D] = eig(A);
+    kernel_index = find(abs(diag(D))<tol==1);
+    null_space = V(:,kernel_index)
+       
+    %%% QR decomposition
+    [~,R] = qr(transpose(A));
+    nullity_QR = sum(abs(diag(R)) < tol);
+
+
+end  
--- Newton Method/D_FiniteDiff.m
+++ Newton Method/D_FiniteDiff.m
@@ -3,17 +3,17 @@ function DF = D_FiniteDiff(F,x, tol)
 % Nov 2021, Miguel Ayala.
 % ================================================
 
-    x = reshape(x, (:, 1));
+    x = reshape(x, [], 1);
 
     h = tol;
     m = length(x);
-    E = Matrix(1.0I, m, m);
+    E = eye(m, m);
 
     DF = zeros(m,m);
 
-    for j in 1:m
-        xh = x + h*E[:,j];
-        DF[:,j] = (F(xh) - F(x) )/h;
+    for j = 1:m
+        xh = x + h*E(:,j);
+        DF(:,j) = (F(xh) - F(x) )/h;
     end
     
 end
