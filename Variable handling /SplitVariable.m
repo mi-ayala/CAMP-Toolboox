a85c5fa8452d1eaf6fe775fb17ba9338fc97207e
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-02-11 20:24:00 -0500

    Fixing bugs

--- Variable handling /SplitVariable.m
+++ Variable handling /SplitVariable.m
@@ -4,37 +4,40 @@ function  [core, tail] = SplitVarible(x, m_truncated, structure)
 %  ==================================================
 %  DESCRIPTION Split variable in core and tail.
 %  __________________________________________________
-%  INPUT
+%  INPUT 
+%  x .. Full extended variable
+%  m_tuncrated .. truncation modes
+%  structure .. 
+%  [number of extraVar,  number of seqVar, 2] for 2-sided seq.
+%  [number of extraVar,  number of seqVar, 1] for 1-sided seq.
 %  __________________________________________________
 %  OUTPUT
+%  core .. core part of the variable
+%  tail .. tail part of the variable
 %  __________________________________________________
 %  MATLAB 9.11.0.1837725 (R2021b) Update 2
 %  Miguel Ayala, 10-Feb-2022.
 %  ==================================================
 
-[var ~ m] = StructureVariable(x, structure)
-tail_size = m - m_truncated;
-numVar = structure(2);
+    [var extravar m] = StructureVariable(x, structure);
+    tail_size = m - m_truncated;
+    numVar = structure(2);
 
-tail = [ones( tail_ size ,numVar) zeros( 2*m_truncated, tail_ size ,numVar) ones( tail_ size ,numVar) ];
-core = ~tail;
+    %%% 2-sided
+    if structure(3) == 2
 
+        tail = [ones( tail_size ,numVar); zeros( 2*m_truncated + 1,numVar); ones( tail_size ,numVar) ];
+        core = ~tail;
+        
+        core = var.*core;
+        tail = var.*tail;
 
-%%% Two sided
-if structure(3) == 2
+    else
+    %%% 1-sided    
+        tail = [zeros( 2*m_truncated + 1,numVar); ones( tail_size ,numVar) ];
+        core = ~tail;
 
-    tail = [ones( tail_ size ,numVar) zeros( 2*m_truncated, tail_ size ,numVar) ones( tail_ size ,numVar) ];
-    core = ~tail;
-    
-    core = var.*core;
-    tail = var.*tail;
-
-else
-
-    tail = [zeros( 2*m_truncated, tail_ size ,numVar) ones( tail_ size ,numVar) ];
-    core = ~tail;
-
-    core = var.* core;
-    tail = var.*tail;
+        core = var.* core;
+        tail = var.*tail;
 
 end  
--- Variable handling /StructureVariable.m
+++ Variable handling /StructureVariable.m
@@ -5,26 +5,33 @@ function  [var extraVar m] = StructureVariable(x, structure)
 %  DESCRIPTION To better deal with extended variables
 %  __________________________________________________
 %  INPUT
+%  x .. Full extended variable
+%  structure .. 
+%  [number of extraVar,  number of seqVar, 2] for 2-sided seq.
+%  [number of extraVar,  number of seqVar, 1] for 1-sided seq.
 %  __________________________________________________
 %  OUTPUT
+%  var .. Sequence variables  by columns
+%  extraVar .. extra variables vector
+%  m .. number of modes, not including zero
 %  __________________________________________________
 %  MATLAB 9.11.0.1837725 (R2021b) Update 2
 %  Miguel Ayala, 11-Feb-2022.
 %  ==================================================
   
-ExtraVar = structure(1);
-SequenceVar = structure(2):
-SequenceStructure = structure(3);
+    ExtraVar = structure(1);
+    SequenceVar = structure(2);
+    SequenceStructure = structure(3);
 
-var = reshape(x(ExtraVar + 1, end), SequenceVar, []);
-extraVar = x(1:ExtraVar);
+    var = reshape(x(ExtraVar + 1: end), [],SequenceVar);
+    extraVar = x(1:ExtraVar);
 
-%%% Number of modes, not including zero mode.
-if SequenceStructure == 1
-    m = length(var(:,1)) - 1;
-else  
-    m = (length(var(:,1)) - 1)/2;
-end    
+    %%% Number of modes, not including zero mode.
+    if SequenceStructure == 1
+        m = length(var(:,1)) - 1;
+    else  
+        m = (length(var(:,1)) - 1)/2;
+    end    
 
 
 end  
