51a8ad4a6db8b8bfe086fe54f4b52b1caac243bf
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-02-13 20:00:28 -0500

    Norm update

---
+++ Norms/Operator_norm.m
@@ -0,0 +1,43 @@
+
+function  Norm = Operator_norm(a, nu, varargin)
+%  ==================================================
+%  l1_norm
+%  ==================================================
+%  DESCRIPTION 
+%  Computes {l^1}^n geometric-weighted norm.
+%  If a is matrix, it computes the norm by columns.
+%  __________________________________________________
+%  INPUT
+%  a .. sequence
+%  nu .. weight
+%  varargin ..  1 for one-sided seq.
+%               2 for two sided seq.
+%  __________________________________________________
+%  OUTPUT
+%  Norm ..  l1 geometric-weighted norm of a
+%  __________________________________________________
+%  MATLAB 9.11.0.1837725 (R2021b) Update 2
+%  Miguel Ayala, 08-Feb-2022.
+%  ==================================================
+
+    %%% Number of modes
+    N = length(a(:,1))-1;
+
+    %%% Geometric weight
+
+    if isempty(varargin)
+        index = 0:N;
+    else
+        N = (N-1)/2;
+        index = -N:N;
+        %index = varargin;
+    end          
+
+    exponent = repmat(index', 1, length(a(1,:)) );
+    w = nu*ones(size(a));
+
+    %%% Building output  
+    Norm = sum( abs(a).* (w.^exponent) , 1);
+
+
+end  
